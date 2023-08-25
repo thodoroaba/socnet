3adea5fe0850c4b833d22eedf85f277a22b83c34
Tiberius Hodoroaba <tiberius.hodoroaba@gmail.com>
2023-08-25 10:28:40 +0300

    init

---
+++ src/test/java/com/socnet/SocnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
